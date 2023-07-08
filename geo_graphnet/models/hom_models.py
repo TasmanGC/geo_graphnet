d753893edd613248ad905525f833f0e313ed6f15
TasmanGC <40192099+TasmanGC@users.noreply.github.com>
2023-07-08 14:40:06 +0800

    🚧 - Port Homogenous Models from dev

--- geo_graphnet/models/hom_models.py
+++ geo_graphnet/models/hom_models.py
@@ -3,14 +3,43 @@ from custom_layers import WGraphConv
 import torch.nn.functional as F
 import torch.nn as nn
 import dgl
+from geo_graphnet.data_handling.config_handlers import GeoGraphConfig
 
+def initialise_model(input:GeoGraphConfig):
+    if input.type != 'model':
+        raise ValueError('Received non model config.')
+    else:
+        mod_type = GeoGraphConfig.model_type
+        
+        if mod_type=='gcn':
+            in_feats = GeoGraphConfig.in_feats
+            hid_feats = GeoGraphConfig.hid_feats
+            num_classes = GeoGraphConfig.num_classes
+            return(GCN(in_feats,hid_feats,num_classes))    
+        
+        if mod_type=='gat':
+            in_feats = GeoGraphConfig.in_feats
+            hid_feats = GeoGraphConfig.hid_feats
+            num_classes = GeoGraphConfig.num_classes
+            num_heads = GeoGraphConfig.num_heads
+            return(GAT(in_feats, hid_feats, num_classes, num_heads))
+        
+        if mod_type=='wgcn':
+            in_feats = GeoGraphConfig.in_feats
+            hid_feats = GeoGraphConfig.hid_feats
+            num_classes = GeoGraphConfig.num_classes
+            return(WGCN(in_feats, hid_feats, num_classes))
+        
+        else:
+            raise ValueError(f'Behaviour Undefined for model type {mod_type} was expecting one of [gcn,gat,wgcn]')
+    
 class GCN(nn.Module):
-    def __init__(self, in_feats:int, h_feats:int, num_classes:int=2):
+    def __init__(self, in_feats:int, hid_feats:int, num_classes:int):
         super(GCN, self).__init__()
-        self.conv1 = GraphConv(in_feats, h_feats)
-        self.conv2 = GraphConv(h_feats, num_classes)
+        self.conv1 = GraphConv(in_feats, hid_feats)
+        self.conv2 = GraphConv(hid_feats, num_classes)
 
-    def forward(self, g:dgl.graph, in_feat):
+    def forward(self, g:dgl.graph, in_feat):# TODO these forward methods should always just take a graph in
         h = self.conv1(g, in_feat)
         h = F.relu(h)
         h = self.conv2(g, h)
@@ -18,29 +47,29 @@ class GCN(nn.Module):
         return h
 
 class WGCN(nn.Module):
-    def __init__(self, in_feats, h_feats, num_classes):
+    def __init__(self, in_feats, hid_feats, num_classes):
         super(WGCN, self).__init__()
-        self.conv1 = WGraphConv(in_feats, h_feats)
-        self.conv2 = WGraphConv(h_feats, num_classes)
+        self.conv1 = WGraphConv(in_feats, hid_feats)
+        self.conv2 = WGraphConv(hid_feats, num_classes)
 
-    def forward(self, g, in_feat, edge_weight):
+    def forward(self, g, in_feat, edge_weight): # TODO these forward methods should always just take a graph in
         # Apply graph convolution and activation.
-        h = self.conv1(g, in_feat,   edge_weight)
+        h = self.conv1(g, in_feat, edge_weight)
         h = F.relu(h)
-        h = self.conv2(g, h,         edge_weight)
+        h = self.conv2(g, h, edge_weight)
         h = F.softmax(h,dim=1)
         return h
 
 class GAT(nn.Module):
-    def __init__(self, in_feats, hid_feat , out_feats, num_heads):
+    def __init__(self, in_feats, hid_feat, num_classes, num_heads):
         super(GAT, self).__init__()
         self.layer1 = GATConv(in_feats, hid_feat, num_heads)
         # Be aware that the input dimension is hidden_dim*num_heads since
         # multiple head outputs are concatenated together. Also, only
         # one attention head in the output layer.
-        self.layer2 = GATConv(hid_feat*num_heads, out_feats, 1)
+        self.layer2 = GATConv(hid_feat*num_heads, num_classes, 1)
 
-    def forward(self, g, h):
+    def forward(self, g, h): # TODO these forward methods should always just take a graph in
         h,a_1 = self.layer1(g, h, get_attention=True)
         # Concat last 2 dim (num_heads * out_dim)
         h = h.view(-1, h.size(1) * h.size(2)) # (in_feat, num_heads, out_dim) -> (in_feat, num_heads * out_dim)
